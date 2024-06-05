7ed21c930b406fad71ba28c9b0ae95aa00a2f1c4
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-06-05 08:35:27 -0300

    aplicação blog

--- app/controllers/posts/posts_controller.rb
+++ app/controllers/posts/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     # displayed in the Index view (index.html.erb)
-    # temos o controlador solicitando algo ao modelo (“Ei, me dê todas as postagens!”), empacotando-as em uma variável de instância @posts
+    # temos o controlador solicitando algo ao modelo
+    # (“Ei, me dê todas as postagens!”), empacotando-as em uma variável de instância @posts
   end
 
   def new
@@ -17,30 +18,47 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(allowed_post_params)
     if @post.save
-        flash[:success] = "Great! Your post has been created!"
-        redirect_to post_path(@post.id)
-        # redirect_to @post
+      flash[:success] = 'Great! Your post has been created!'
+      redirect_to post_path(@post.id)
+    # redirect_to @post
     else
-        flash.now[:error] = "Rats! Fix your mistekes, please"
-        render :new, status: :unprocessable_entity 
+      flash.now[:error] = 'Rats! Fix your mistekes, please'
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def show; end
-
-  def edit; end
-
-  def update; end
-
-  def destroy; end
-
-  private
-
-  def allowed_post_params
-    params.require(:post).permit(:title, :body, :author_id)
+  # edit sempre antes do show
+  def edit
+    @post = Post.find(params[:id])
   end
 
-  # Se você não fizesse isso, quando tentasse acessar params[:post] nada apareceria! 
-  # Além disso, se houver campos adicionais enviados dentro do hash, eles serão removidos e ficarão inacessíveis (para protegê-lo).
+  def show
+    @post = Post.find(params[:id])
+  end
 
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(allowed_post_params)
+      redirect_to post_path
+    else
+      render :edit, status: unprocessable_entity
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+
+      redirect_to post_path, status: :see_other
+    end
+
+    private
+
+    def allowed_post_params
+      params.require(:post).permit(:title, :body, :author_id)
+    end
+
+    # Se você não fizesse isso, quando tentasse acessar params[:post] nada apareceria!
+    # Além disso, se houver campos adicionais enviados dentro do hash, eles serão removidos e ficarão inacessíveis (para protegê-lo).
+  end
 end
