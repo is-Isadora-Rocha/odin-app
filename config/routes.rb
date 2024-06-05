bf0206f83bb07ef4bc58e45c420985880e88126e
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-06-05 11:15:21 -0300

    routes e controller articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  get "/articles", to: "articles#index" #A rota declara que GET /articles as solicitações são mapeadas para a index ação de ArticlesController.
 
-  # Para criar ArticlesController e sua indexação, executaremos o gerador do controlador (com a --skip-routesopção porque já temos uma rota apropriada)
-  # bin/rails generate controller Articles index --skip-routes
-
+  root 'articles#index'
+  get '/articles', to: 'articles#index'
 end
